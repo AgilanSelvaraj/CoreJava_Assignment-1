131d2e0aea0f93043fe0fa78becd6f95abd885dc
AgilanSelvaraj <agilan.ks07@gmail.com>
2016-07-19 18:34:12 +0530

    Sum of two numbers

---
+++ Sum_of_two_numbers.java
@@ -0,0 +1,14 @@
+package introduction_to_java.Assignment_1;
+
+public class Sum_of_two_numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum, a = 10;
+		float b = 3;
+		
+		sum = a + (int)b;
+		System.out.println(sum);
+	}
+
+}
